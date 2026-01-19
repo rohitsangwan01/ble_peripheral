0b038292ced07effcae1d10b570e8bfc74ceaf69
Farouk Boussarsar <fboussarsar@tn.int.habemus.com>
2026-01-19 17:11:21 +0100

    Fix Windows build config and radio detection (v2.4.7)

--- example/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ example/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/fboussarsar/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/fboussarsar/Desktop/dev/ble_peripheral/example"
+export "FLUTTER_ROOT=C:\Users\fboussarsar\Documents\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\fboussarsar\Desktop\dev\ble_peripheral\example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=/Users/fboussarsar/Desktop/dev/ble_peripheral/example/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
-export "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuMzUuNQ==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049YWM0ZTc5OWQyMw==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049ZDNkNDVkY2YyNQ==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My45LjI="
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=/Users/fboussarsar/Desktop/dev/ble_peripheral/example/.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
